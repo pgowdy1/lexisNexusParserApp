7938335b38d0871e1771e08af8a8f5e9b1d7ad1b
Perry Gowdy <pgowdy1@gmail.com>
2018-07-14 14:58:25 -0500

    Checking visual studio integration.

--- Form1.cs
+++ Form1.cs
@@ -24,7 +24,7 @@ namespace krisParserApp
 
         private void label1_Click(object sender, EventArgs e)
         {
-
+            //Testing VS Studio commit.
         }
 
         private void exitButton_Click(object sender, EventArgs e)
